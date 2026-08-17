c1d924a068d80d4a159372bbc4fb7a16a9133e1a
Devarsh-Jhaveri <devarsh.jhaveri02@gmail.com>
2026-08-17 23:58:45 +0530

    Add Pattern6.java to print a specific pattern

---
+++ Patterns/Pattern6.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+public class Pattern6{
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Enter your no.");
+        int n=Integer.parseInt(scn.nextLine().trim());
+        int sp=1, st=(n/2)+1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=st;j++){
+                System.out.print("*\t");
+            }
+            for(int j=1;j<=sp;j++){
+                System.out.print("\t");
+            }
+            for(int j=1;j<=st;j++){
+                System.out.print("*\t");
+            }
+            System.out.println("");
+            if (i<=n/2){
+                sp+=2; st--;
+            }
+            else{
+                sp-=2; st++;
+            }
+        }     
+    }
+}
