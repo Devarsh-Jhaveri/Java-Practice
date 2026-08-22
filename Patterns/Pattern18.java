f9dc9143291913b53db390c881b68f3c42035880
Devarsh-Jhaveri <devarsh.jhaveri02@gmail.com>
2026-08-22 10:39:15 +0530

    Add Pattern18.java to print a specific pattern

---
+++ Patterns/Pattern18.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Pattern18{
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        int n=Integer.parseInt(scn.nextLine());
+        int st=n, sp=0, is=n-4;
+        
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=sp;j++){
+                System.out.print("\t");
+            }
+            for(int j=1; j<=st;j++){
+                if(i>1 && i<=n/2 && j>1 && j<st){
+                    System.out.print("\t");
+                }
+                else{
+                    System.out.print("*\t");
+                }
+            }
+            if(i<=n/2){
+                st-=2; sp++;
+            }
+            else{
+                st+=2; sp--;
+            }
+            System.out.println("");
+        }
+
+    }
+}
